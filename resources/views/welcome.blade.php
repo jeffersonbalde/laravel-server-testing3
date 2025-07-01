fcc95d1714c0e4789286481eec612847aeb56fbe
jeffersonbalde <jeffersonbalde8@gmail.com>
2025-07-02 01:41:45 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Jefferson Balde
+                    Laravel Jefferson Sabanal Balde
                 </div>
 
                 <div class="links">
